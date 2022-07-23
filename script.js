53364a36f67f52cefd156445a9f8cb57a0d77ff2
itsechi <wardulinskao@gmail.com>
2022-07-23 18:18:25 +0200

    Add a function to toggle darkmode

--- script.js
+++ script.js
@@ -1,10 +1,32 @@
- const toggleSidebarBtn = document.querySelector('#toggle-sidebar');
- const nav = document.querySelector('nav');
+const toggleSidebarBtn = document.querySelector("#toggle-sidebar");
+const toggleDarkmodeBtn = document.querySelector("#darkmode-toggle");
+const nav = document.querySelector("nav");
+const chart = document.querySelector("#chart");
+const logo = document.querySelector("#logo");
+const body = document.querySelector("body");
+let darkmode = false;
 
-toggleSidebarBtn.addEventListener('click', toggleSidebar);
+toggleSidebarBtn.addEventListener("click", toggleSidebar);
+toggleDarkmodeBtn.addEventListener("click", toggleDarkmode);
 
+function toggleSidebar() {
+  nav.classList.toggle("close");
+  console.log("Hi");
+}
 
- function toggleSidebar() {
-  nav.classList.toggle('close');
-  console.log('Hi');
- }
+function toggleDarkmode() {
+  body.classList.toggle("dark");
+  if (!darkmode) {
+    darkmode = true;
+    chart.innerHTML = '<img src="./img/chart-dark.svg" alt="chart">';
+    logo.innerHTML =
+      '<img src="./img/logo-dark.svg" alt="logo" class="logo-img" width="50"><h1>dashboard</h1>';
+    toggleDarkmodeBtn.innerHTML = '<i class="uil uil-toggle-on"></i>';
+  } else if (darkmode) {
+    darkmode = false;
+    chart.innerHTML = '<img src="./img/chart.svg" alt="chart">';
+    logo.innerHTML =
+      '<img src="./img/logo.svg" alt="logo" class="logo-img" width="50"><h1>dashboard</h1>';
+    toggleDarkmodeBtn.innerHTML = '<i class="uil uil-toggle-off"></i>';
+  }
+}
